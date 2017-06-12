06f6181140b575006a4ebfa8d27dad6ab598dc7d
黄宸 <huangchen@iot.chinamobile.com>
2017-06-12 10:53:30 +0800

    Initial commit

---
+++ src/main/java/com/quiterr/App.java
@@ -0,0 +1,10 @@
+package com.quiterr;
+
+/**
+ * Created by Huangchen on 2017/6/12.
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("hi");
+    }
+}
